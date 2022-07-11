458e63ffcf76d1186952c2765005fa1a6c4ce112
mttaggart <mtaggart@taggart-tech.com>
2022-07-10 18:58:47 -0700

    Update demo TF

--- Terraform/single-workstation/main.tf
+++ Terraform/single-workstation/main.tf
@@ -7,22 +7,29 @@ terraform {
   }
 }
 
+variable proxmox_host {
+  type        = string
+  default     = "starbase"
+  description = "description"
+}
+
+
+
 provider "proxmox" {
   # Configuration options
-  pm_api_url          = "https://192.168.1.50:8006/api2/json"
-  pm_api_token_id     = "mttaggart@pam!mttaggart"
-  pm_api_token_secret = "9525cd7a-66cc-4df9-9bd3-f87e9b0ca2d3" 
+  pm_api_url          = "https://${var.proxmox_host}:8006/api2/json"
   pm_tls_insecure     = true
   pm_log_enable       = true
 }
 
 resource "proxmox_vm_qemu" "demo-ws" {
   cores       = 2
-  memory       = 4096
+  memory      = 4096
   name        = "WS-TF-Demo"
   target_node = "starbase"
   clone       = "seclab-win-ws"
   full_clone  = false
+  agent       = 1
 
   disk {
     type         = "virtio"
@@ -46,4 +53,10 @@ output "demo-id" {
   value       = proxmox_vm_qemu.demo-ws.id
   sensitive   = false
   description = "VM ID"
+}
+
+output "demo-ip" {
+  value       = proxmox_vm_qemu.demo-ws.default_ipv4_address
+  sensitive   = false
+  description = "VM IP"
 }
