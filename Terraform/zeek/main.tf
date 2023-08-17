a82dbac783c54f331017c17097f8a21adeb10885
Taggart <mtaggar@taggart-tech.com>
2023-08-17 06:26:13 -0700

    Add Zeek

---
+++ Terraform/zeek/main.tf
@@ -0,0 +1,95 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "Telmate/proxmox"
+      version = "2.9.13"
+    }
+    vault = {
+      source = "hashicorp/vault"
+      version = "3.16.0"
+    }
+  }
+}
+
+variable "proxmox_host" {
+  type        = string
+  default     = "starbase"
+  description = "description"
+}
+
+variable "hostname" {
+  type        = string
+  default     = "seclab-zeek"
+  description = "description"
+}
+
+provider "vault" {
+
+}
+
+data "vault_kv_secret_v2" "seclab" {
+  mount = "seclab"
+  name  = "seclab"
+}
+
+provider "proxmox" {
+  # Configuration options
+  pm_api_url      = "https://${var.proxmox_host}:8006/api2/json"
+  pm_tls_insecure = true
+  pm_log_enable   = true
+  pm_api_token_id = data.vault_kv_secret_v2.seclab.data.proxmox_api_id
+  pm_api_token_secret = data.vault_kv_secret_v2.seclab.data.proxmox_api_token
+}
+
+
+resource "proxmox_vm_qemu" "seclab-zeek" {
+  cores       = 4
+  memory      = 7192
+  name        = "Seclab-Zeek"
+  target_node = var.proxmox_host
+  clone       = "seclab-ubuntu-server-22-04"
+  full_clone  = false
+  onboot      = true
+  agent       = 1
+
+  connection {
+    type = "ssh"
+    user = data.vault_kv_secret_v2.seclab.data.seclab_user
+    password = data.vault_kv_secret_v2.seclab.data.seclab_password
+    host = self.default_ipv4_address
+  }
+
+  network {
+    bridge = "vmbr2"
+    model  = "e1000"
+  }
+  network {
+    bridge = "vmbr1"
+    model  = "e1000"
+  }
+
+  provisioner "file" {
+    source      = "./00-netplan.yaml"
+    destination = "/tmp/00-netplan.yaml"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo sed -i 's/seclab-ubuntu22/${var.hostname}/g' /etc/hosts",
+      "sudo sed -i 's/seclab-ubuntu22/${var.hostname}/g' /etc/hostname",
+      "sudo mv /etc/netplan/00-installer-config.yaml /etc/netplan/00-installer-config.yaml.bak",
+      "sudo mv /tmp/00-netplan.yaml /etc/netplan/00-netplan.yaml",
+      "sudo hostname ${var.hostname}",
+      "sudo netplan apply && sudo ip addr add dev ens18 ${self.default_ipv4_address}",
+      "ip a s"
+    ]
+  }
+
+
+}
+
+output "vm_ip" {
+  value       = proxmox_vm_qemu.seclab-zeek.default_ipv4_address
+  sensitive   = false
+  description = "VM IP"
+}
