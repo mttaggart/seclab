c74ffdf17cd39ddb058bd83b470a4583d0c7186a
Michael Taggart <mtaggart@taggart-tech.com>
2023-08-28 22:14:57 -0700

    Add missing hashicorp repo installation

--- jumpbox_setup.sh
+++ jumpbox_setup.sh
@@ -20,6 +20,7 @@ install_vivaldi() {
 install_hashicorp() {
 	echo "[+] Setting up Hashicorp Repository"
 	wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 	sudo apt update
 	echo "[+] Installing Hashicorp Tools"
 	sudo apt install -y packer terraform vault
