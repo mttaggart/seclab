affc1ce8df02ce1e48f7fa0b53702ef9f2c96702
Taggart <mtaggar@taggart-tech.com>
2023-09-05 00:15:39 -0700

    Copy example files to live in init-cloud-init.sh

--- Packer/init-cloud-init.sh
+++ Packer/init-cloud-init.sh
@@ -4,18 +4,22 @@ echo "[+] Extracting Vault Secrets"
 printf "[?] Login to Vault? [y/N]"
 read vault_login
 if [[ $vault_login == "y" ]]; then
-    vault login
+	vault login
 fi
 seclab_user=$(vault kv get -field=seclab_user seclab/seclab)
 seclab_pw=$(vault kv get -field=seclab_password seclab/seclab)
 seclab_ssh_key=$(vault kv get -field=seclab_ssh_key seclab/seclab)
 encrypted_pw=$(openssl passwd -6 $seclab_pw)
+echo "[+] Moving example files to active files"
+for f in $(find ./ -name "user-data.example" -or -name "*.preseed.example"); do
+	cp $f "${f%.example}"
+done
 echo $encrypted_pw
 echo "[+] Adding encrypted secret to user-data/preseed files"
-for f in $(find ./ -name user-data -or -name "*.preseed"); do
-    cp $f $f.bak
-    sed -i "s/SECLAB_USER/$seclab_user/g" $f
-    sed -i "s:SECLAB_PASSWORD:${encrypted_pw}:g" $f
-    sed -i "s:SECLAB_SSH_KEY:$seclab_ssh_key:g" $f
+for f in $(find ./ -name "user-data" -or -name "*.preseed"); do
+	cp $f $f.bak
+	sed -i "s/SECLAB_USER/$seclab_user/g" $f
+	sed -i "s:SECLAB_PASSWORD:${encrypted_pw}:g" $f
+	sed -i "s:SECLAB_SSH_KEY:$seclab_ssh_key:g" $f
 done
 exit 0
