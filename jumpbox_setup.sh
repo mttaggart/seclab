9620c66cebad8f5ec53ce26df9d812e78b982ad4
Michael Taggart <mtaggart@taggart-tech.com>
2024-04-15 20:38:58 -0700

    /dev/null for hashi key

--- jumpbox_setup.sh
+++ jumpbox_setup.sh
@@ -30,7 +30,7 @@ install_vivaldi() {
 
 install_hashicorp() {
 	echo "[+] Setting up Hashicorp Repository"
-	wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+	wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg >/dev/null
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 	sudo apt update
 	echo "[+] Installing Hashicorp Tools"
