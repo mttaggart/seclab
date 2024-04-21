b3a3e25c3bdab00190cdd3ded890b4fa05e45737
Michael Taggart <mtaggart@taggart-tech.com>
2024-04-21 00:06:22 -0700

    Handle b64-encoded windows passwords

--- Packer/mkiso.sh
+++ Packer/mkiso.sh
@@ -11,12 +11,27 @@ fi
 seclab_user=$(vault kv get -field=seclab_user seclab/seclab)
 windows_pw=$(vault kv get -field=seclab_windows_password seclab/seclab)
 windows_domain_pw=$(vault kv get -field=seclab_windows_domain_password seclab/seclab)
+
+# Necessary appends for b64-encded passwords because...reasons?
+windows_userpw=$windows_pw"Password"
+windows_adminpw=$windows_pw"AdministratorPassword"
+windows_domain_adminpw=$windows_domain_pw"AdministratorPassword"
+
+# Convert password to Windows b64
+windows_userpw=$(echo -n $windows_userpw | iconv -f utf-8 -t utf16le | base64 -w0)
+windows_adminpw=$(echo -n $windows_adminpw | iconv -f utf-8 -t utf16le | base64 -w0)
+windows_domain_adminpw=$(echo -n $windows_domain_adminpw | iconv -f utf-8 -t utf16le | base64 -w0)
+
+
 echo "[+] Backing up Autounattend file"
 cp -R $1 $1.bak
+
 echo "[+] Injecting password"
-sed -i "s/SECLAB_WINDOWS_PASSWORD/$windows_pw/g" $1/autounattend.xml
-sed -i "s/SECLAB_WINDOWS_DOMAIN_PASSWORD/$windows_domain_pw/g" $1/autounattend.xml
+sed -i "s/SECLAB_WINDOWS_PASSWORD/$windows_userpw/g" $1/autounattend.xml
+sed -i "s/SECLAB_WINDOWS_ADMIN_PASSWORD/$windows_adminpw/g" $1/autounattend.xml
+sed -i "s/SECLAB_WINDOWS_DOMAIN_PASSWORD/$windows_domain_adminpw/g" $1/autounattend.xml
 sed -i "s/SECLAB_USER/$seclab_user/g" $1/autounattend.xml
+
 echo "[+] Making ISO"
 new_iso="Autounattend-$(basename $PWD).iso"
 genisoimage -J -l -R -V "Autounattend" -iso-level 4 -o ../$new_iso $1
