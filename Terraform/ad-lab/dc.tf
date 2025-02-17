28dd656bd90b024ab59f0c316f822269298e2e2a
Taggart <mtaggart@taggart-tech.com>
2025-02-16 21:31:25 -0800

    AD Lab:Fix SSH conn

--- Terraform/ad-lab/dc.tf
+++ Terraform/ad-lab/dc.tf
@@ -47,7 +47,7 @@ resource "proxmox_virtual_environment_vm" "zd-dc" {
     type            = "ssh"
     user            = data.keepass_entry.seclab_windows.username
     password        = data.keepass_entry.seclab_windows.password
-    host            = self.ipv4_addresses[0][0]
+    host            = self.ipv4_addresses[1][0]
     target_platform = "windows"
   }
 
