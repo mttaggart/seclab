04cf6d17ec63fea5a72342d19918d04ddf92dde8
Taggart <mtaggart@taggart-tech.com>
2025-06-06 08:21:38 -0700

    Fix outbound Firewall for DMZ

--- Terraform/dmz-relay/firewall.tf
+++ Terraform/dmz-relay/firewall.tf
@@ -1,29 +1,70 @@
 resource "digitalocean_firewall" "dmz-relay-fw" {
-  name = "dmz-relay"
+  name        = "dmz-relay"
   droplet_ids = [digitalocean_droplet.dmz-relay.id]
 
   inbound_rule {
-    protocol = "udp"
-    port_range = "${var.wg_port}"
-    source_addresses = ["0.0.0.0/0", "::/0"]
-  }
-  
-  inbound_rule {
-    protocol = "tcp"
-    port_range = "80"
-    source_addresses = ["0.0.0.0/0", "::/0"]
-  }
-  
-  inbound_rule {
-    protocol = "tcp"
-    port_range = "443"
+    protocol         = "udp"
+    port_range       = var.wg_port
     source_addresses = ["0.0.0.0/0", "::/0"]
   }
 
   inbound_rule {
-    protocol = "tcp"
-    port_range = "22"
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
     source_addresses = ["${data.http.ipify.response_body}"]
   }
-  
+
+  inbound_rule {
+    protocol         = "icmp"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "80"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "443"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "51888"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+
 }
